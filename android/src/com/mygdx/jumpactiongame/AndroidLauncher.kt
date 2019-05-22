3dd55c304005a43573d60aae8ac0789087a0bef0
hitorigoe <torigoe_0207@yahoo.co.jp>
2019-05-22 16:04:14 +0900

    新規作成

---
+++ android/src/com/mygdx/jumpactiongame/AndroidLauncher.kt
@@ -0,0 +1,15 @@
+package com.mygdx.jumpactiongame
+
+import android.os.Bundle
+
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import com.mygdx.jumpactiongame.JumpActionGame
+
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val config = AndroidApplicationConfiguration()
+        initialize(JumpActionGame(), config)
+    }
+}
